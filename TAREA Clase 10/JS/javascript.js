20cf9cd3f1d44d58f5e50d1ef106aa14a0a9aad3
licsergioantuna <124640783+licsergioantuna@users.noreply.github.com>
2023-03-05 23:57:38 -0300

    Add files via upload

---
+++ TAREA Clase 10/JS/javascript.js
@@ -0,0 +1,19 @@
+var nombre = "Sergio";
+var apellido = "Antuna";
+
+console.log(nombre + " " + apellido);
+
+
+var numeroA = 3;
+var numeroB = 7;
+var suma = numeroA + numeroB;
+
+console.log(numeroA + numeroB);
+
+
+var cuatro = suma + 5;
+
+console.log (cuatro);
+
+
+
